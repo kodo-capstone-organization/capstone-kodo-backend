6962eb5c9ab8223f803dcc6e246ba2bf1fa77bab
Theodoric <theodorickeithlim@gmail.com>
2021-08-20 15:04:56 +0800

    Init SpringBoot

---
+++ src/main/java/com/spring/kodo/KodoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodoApplication.class, args);
+	}
+
+}
