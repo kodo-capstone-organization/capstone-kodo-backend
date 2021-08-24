74eaecd7be69c7b200b6e7cf587aaf8363efedd1
FluffDucks <fluffducks@outlook.com>
2021-08-24 18:08:27 +0800

    Setup configs to connect to GCS for file upload

---
+++ src/main/java/com/spring/kodo/config/GCSConfig.java
@@ -0,0 +1,26 @@
+package com.spring.kodo.config;
+
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.cloud.storage.Storage;
+
+@Configuration
+public class GCSConfig
+{
+    @Value("${google.cloud.storage.bucket.name}")
+    private String bucketName;
+
+    public Storage getGCSClient()
+    {
+        // Get service by env var GOOGLE_APPLICATION_CREDENTIALS (stores path to json key file)
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        return storage;
+    }
+
+    public String getBucketName()
+    {
+        return bucketName;
+    }
+}
