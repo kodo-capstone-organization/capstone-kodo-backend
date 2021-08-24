b29dd059434f2c694338b19701b9537ed5c64d4a
Theodoric <theodorickeithlim@gmail.com>
2021-08-24 14:52:38 +0800

    Add LessonRepository

---
+++ src/main/java/com/spring/kodo/repository/LessonRepository.java
@@ -0,0 +1,23 @@
+package com.spring.kodo.repository;
+
+import com.spring.kodo.entity.Account;
+import com.spring.kodo.entity.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LessonRepository extends JpaRepository<Lesson, Long>
+{
+    // Basically the EntityManager of Account
+    // Provides methods out-of-the-box. E.g.:
+    // 1 - save(S entity)
+    // 2 - findById(ID id)
+    // 3 - findOne()
+    // 4 - findAll()
+    // For full method list, ctrl + click into JpaRepository
+    // Or visit: https://docs.spring.io/autorepo/docs/spring-data-jpa/current/api/org/springframework/data/jpa/repository/support/SimpleJpaRepository.html
+
+    Optional<Lesson> findByName(String name);
+}
