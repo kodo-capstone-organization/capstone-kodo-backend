457fe4c224d48377172494d81993facac8811681
Bryson Tan <brysontzy@hotmail.com>
2021-09-12 11:00:42 +0800

    Updated getNumberOfStudentAttemptsLeft api to use path variable

--- src/main/java/com/spring/kodo/controller/StudentAttemptController.java
+++ src/main/java/com/spring/kodo/controller/StudentAttemptController.java
@@ -5,7 +5,7 @@ import com.spring.kodo.util.exception.AccountNotFoundException;
 import com.spring.kodo.util.exception.QuizNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotat.ion.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
