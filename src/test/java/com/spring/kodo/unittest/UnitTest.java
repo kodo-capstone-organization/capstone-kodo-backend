70b07d722a8723ea3a64904d7c396c9ab03f435f
Bryson Tan <brysontzy@hotmail.com>
2021-10-19 23:32:50 +0800

    Added unit tests for tag service

--- src/test/java/com/spring/kodo/unittest/UnitTest.java
+++ src/test/java/com/spring/kodo/unittest/UnitTest.java
@@ -7,7 +7,8 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         AccountServiceImplUnitTest.class,
         ContentServiceImplUnitTest.class,
-        CourseServiceImplUnitTest.class
+        CourseServiceImplUnitTest.class,
+        TagServiceImplUnitTest.class
 })
 public class UnitTest
 {
